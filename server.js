7df7a3edbec5fa8bf65e80326d922757b0e01333
Mark O'Neil <zeretic@gmail.com>
2020-05-11 16:35:28 -0400

    Added support  for config file

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 //server.js
+const _ = require ('lodash');
+const config = require ('./config.json');
+
 var http = require('http');
 var url = require('url');
 var queryString = require('querystring');
 var os = require('os');
-let name = '_APP_2';
-let version = '1.0';
 let containerId = os.hostname();
 
+// app instance settings
+let name = config.app_name;
+let version = config.app_version;
+let rootPath = config.app_root_path;
+let versionPath = config.app_version_path;
+
+console.log(`config: ${JSON.stringify(config, undefined, config.json_indentation)}`);
 
 var server = http.createServer(function(request, response) {
-  
+
+
+
   var parts = url.parse(request.url); 
   var responseText = "";
 
-  if (parts.pathname === '/app2/version') {
+  if (parts.pathname === (rootPath + versionPath)) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end(version);
-  } else if (parts.pathname.startsWith('/app2')) {
+  } else if (parts.pathname.startsWith(rootPath)) {
     responseText += name + " v" + version + " via hostname/containerId: " + containerId + "\n\n";
     responseText += "Full passed URL: '" + request.url + "'";
 
@@ -25,7 +35,7 @@ var server = http.createServer(function(request, response) {
   } else {
   // error handle unknown path, 200 for demo only should use 404 in production
     response.writeHead(404, {'Content-Type': 'text/plain'}); 
-    response.end("Endpoint '" + parts.pathname + "' not found... try '/app2' or '/app2/version'. Full passed URL: '" + request.url + "'");
+    response.end("Endpoint '" + parts.pathname + "' not found... try " + rootPath + "or '" + rootPath + versionPath + "'. Full passed URL: '" + request.url + "'");
   }
 });
 
