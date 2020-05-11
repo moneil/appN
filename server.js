7a5915912f7e440fd43ddda97d8bcc9827434741
Mark O'Neil <zeretic@gmail.com>
2020-05-11 15:45:12 -0400

    A simpler demo app...

--- server.js
+++ server.js
@@ -1,111 +1,32 @@
-var express = require('express');
-var config = require('./config/config');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('cookie-session');
+//server.js
+var http = require('http');
+var url = require('url');
+var queryString = require('querystring');
+var os = require('os');
+let name = '_APP_2';
+let version = '1.0';
+let containerId = os.hostname();
 
-var index = require('./controllers/routes/index');
-// var systems = require('./controllers/routes/systems');
-//var courses = require('./controllers/routes/courses');
-//var lists = require('./controllers/routes/lists');
-//var logs = require('./controllers/routes/logs');
-//var rest = require('./controllers/routes/rest');
-//var dataSync = require('./controllers/routes/datasync');
 
-var debug = (config.debugMode === 'true')
+var server = http.createServer(function(request, response) {
+  
+  var parts = url.parse(request.url); 
+  var responseText = "";
 
-//set up mongoose
-//determine db path
-var db = process.env.MONGO_URI || config.test_db;
-// Bring Mongoose into the app
-var mongoose = require( 'mongoose' );
-// Create the database connection
-if (debug) console.log("[SERVER.JS]:db: ", db);
-mongoose.connect(db);
+  if (parts.pathname === '/app2/version') {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end(version);
+  } else if (parts.pathname.startsWith('/app2')) {
+    responseText += name + " v" + version + " via hostname/containerId: " + containerId + "\n\n";
+    responseText += "Full passed URL: '" + request.url + "'";
 
-// CONNECTION EVENTS
-// When successfully connected
-mongoose.connection.on('connected', function () {
-  console.log('Mongoose default connection open to ' + db );
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end(responseText);
+  } else {
+  // error handle unknown path, 200 for demo only should use 404 in production
+    response.writeHead(404, {'Content-Type': 'text/plain'}); 
+    response.end("Endpoint '" + parts.pathname + "' not found... try '/app2' or '/app2/version'. Full passed URL: '" + request.url + "'");
+  }
 });
 
-// If the connection throws an error
-mongoose.connection.on('error',function (err) {
-  console.log('Mongoose default connection error: ' + err);
-});
-
-// When the connection is disconnected
-mongoose.connection.on('disconnected', function () {
-  console.log('Mongoose default connection disconnected');
-});
-
-// If the Node process ends, close the Mongoose connection
-process.on('SIGINT', function() {
-  mongoose.connection.close(function () {
-    console.log('Mongoose default connection disconnected through app termination');
-    process.exit(0);
-  });
-});
-
-var app = express();
-
-//added to enable Heroku to handle proper SSL termination
-app.enable('trust proxy');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use(session({
-  name: 'nodeservertest',
-  secret: '282r5O>Dg0hu?A4',
-  resave: false,
-  saveUninitialized: true
-}))
-
-app.use(express.static(path.join(__dirname, '/public')));
-app.use('/datetimepicker', express.static(path.join(__dirname, '/node_modules/angular-bootstrap-datetimepicker/src/')));
-app.use('/moment', express.static(path.join(__dirname, '/node_modules/moment')));
-app.use('/ask', express.static(path.join(__dirname, '/bower_components/angular-spinkit/build')));
-app.use('/dialogs', express.static(path.join(__dirname, '/bower_components/angular-dialog-service/dist')));
-app.use('/uib', express.static(path.join(__dirname, '/node_modules/angular-ui-bootstrap')));
-app.use('/ngSanitize', express.static(path.join(__dirname, '/node_modules/angular-sanitize')));
-app.use('/ngCsv', express.static(path.join(__dirname, '/node_modules/ng-csv/build')));
-app.use('/ngclipboard', express.static(path.join(__dirname, '/node_modules/ngclipboard/dist')));
-app.use('/ngtranslate', express.static(path.join(__dirname, '/node_modules/angular-translate/dist')));
-
-app.use('/', index);
-// app.use('/systems', systems);
-//app.use('/courses', courses);
-//app.use('/lists', lists);
-//app.use('/logs', logs);
-//app.use('/api', rest);
-//app.get('/data-sync/:id', dataSync.regListener);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  console.log('error ' + res.status + ': ' + err.message + '<br />' + err.stack);
-  res.write('error ' + res.status + ': ' + err.message + '<br />' + err.stack);
-  res.send();
-});
-
-module.exports = app;
+server.listen(8080);
