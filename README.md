effe28fc7a0679802196a8497825341f13a0e043
Mark O'Neil <zeretic@gmail.com>
2020-05-11 20:50:32 -0400

    Add env var support

--- README.md
+++ README.md
@@ -10,11 +10,27 @@ AppN is a very basic node.js application used in scenarios where deploying serve
 * npm
 
 ## Usage
+
+1. Pull the project:
 `$ git clone https://github.com/moneil/appn.git 
 `
 
+2. Configuration your instance:
 Edit `config.json` to change the app instance settings
+or use environment variables.
 
-Start appN using 
+	e.g.:
+	
+	`$ export APP_NAME="_APPN"`
+	
+	`$ export APP_VERSION="1.0"`
+	
+	`$ export APP_ROOT_PATH="app1"`
+	
+	`$ export APP_VERSION_PATH="/app1/version"`
+
+3. Start appN using 
 `$ npm start`
+
+
 
--- server.js
+++ server.js
@@ -8,25 +8,27 @@ var os = require('os');
 let containerId = os.hostname();
 
 // app instance settings
-let name = config.app_name;
+config.app_name = process.env.APP_NAME?process.env.APP_NAME:config.app_name;
+config.app_version = process.env.APP_VERSION?process.env.APP_VERSION:config.app_version;
+config.app_path = process.env.APP_PATH?process.env.APP_PATH:config.app_path;
+config.app_version_path = process.env.APP_VERSION_PATH?process.env.APP_VERSION_PATH:config.app_version_path;
+
+let app_name = config.app_name;
 let version = config.app_version;
-let rootPath = config.app_root_path;
+let appPath = config.app_path;
 let versionPath = config.app_version_path;
 
 console.log(`config: ${JSON.stringify(config, undefined, config.json_indentation)}`);
 
 var server = http.createServer(function(request, response) {
-
-
-
   var parts = url.parse(request.url); 
   var responseText = "";
 
-  if (parts.pathname === (rootPath + versionPath)) {
+  if (parts.pathname === (versionPath)) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end(version);
-  } else if (parts.pathname.startsWith(rootPath)) {
-    responseText += name + " v" + version + " via hostname/containerId: " + containerId + "\n\n";
+  } else if (parts.pathname.startsWith(appPath)) {
+    responseText += app_name + " v" + version + " via hostname/containerId: " + containerId + "\n\n";
     responseText += "Full passed URL: '" + request.url + "'";
 
     response.writeHead(200, {'Content-Type': 'text/plain'});
@@ -34,8 +36,8 @@ var server = http.createServer(function(request, response) {
   } else {
   // error handle unknown path, 200 for demo only should use 404 in production
     response.writeHead(404, {'Content-Type': 'text/plain'}); 
-    response.end("Endpoint '" + parts.pathname + "' not found... try " + rootPath + "or '" + rootPath + versionPath + "'. Full passed URL: '" + request.url + "'");
+    response.end("Endpoint '" + parts.pathname + "' not found... try " + appPath + " or '" + versionPath + "'. Full passed URL: '" + request.url + "'");
   }
-});
+});   
 
 server.listen(8080);
